0e4826a6c1acfd754382c1218212dabf433248d0
Lukasz Cena <lbjcena@gmail.com>
2023-05-01 17:35:20 +0200

    range slider handling added, output field showing the password length added, password lenght can be changed by the user

--- script.js
+++ script.js
@@ -9,12 +9,16 @@ const checkLarge = document.querySelector('#largeLetters');
 const checkNumbers = document.querySelector('#numbers');
 const checkSpecial = document.querySelector('#special');
 const btnGenerate = document.querySelector('.generate');
+const sliderLength = document.querySelector('#passwordLength');
+const sliderValue = document.querySelector('output');
+// lets me dynamically watch for user options being selected/deselected
 let state = {
     includeSmall: false,
     includeLarge: false,
     includeNumbers: false,
     includeSpecial: false
 };
+// creates a final string for password gen, based on which options user selects - the string is REMADE every time an option is selected or deselected 
 const updatePassword = () => {
     let finalPassword = '';
     if (state.includeSmall) {
@@ -33,16 +37,19 @@ const updatePassword = () => {
         return finalPassword;
     }
 };
+// takes a final string, generated based on user's selections, and creates a string serving as the password from the selected options. not perfect, sometimes will omit a character type despite it being selected (since its randomized) but i'll fix it later
 let generatePassword = () => {
     let possibleChars = updatePassword();
     let endResult = '';
-    for (let i = 0; i < 12; i++) {
+    let finalLength = parseInt(sliderLength.value);
+    for (let i = 0; i < finalLength; i++) {
         const randomIndex = Math.floor(Math.random() * possibleChars.length);
         endResult += possibleChars[randomIndex];
     }
     result.value = endResult;
     console.log(endResult.length);
 };
+// checks for checkboxes below - not perfect, refreshing the page will refresh the booleans to false, but will NOT change the checked state of the checkboxes itself, will fix later
 checkSmall.addEventListener('change', () => {
     state.includeSmall = !state.includeSmall;
     console.log(`Small enabled : ${state.includeSmall}`);
@@ -59,6 +66,11 @@ checkSpecial.addEventListener('change', () => {
     state.includeSpecial = !state.includeSpecial;
     console.log(`Special enabled: ${state.includeSpecial}`);
 });
+// make password come out when button click - throws an error in the console if no options are selected, but the error doesn't stop the program from working (you can select a character for generating a password, and it'll work fine) so error in the console it is for now
 btnGenerate.addEventListener('click', () => {
     generatePassword();
+});
+// handle showing values in the output field
+sliderLength.addEventListener('input', () => {
+    sliderValue.value = sliderLength.value;
 });
--- script.ts
+++ script.ts
@@ -12,6 +12,11 @@ const checkSpecial: HTMLInputElement | null = document.querySelector('#special')
 
 const btnGenerate: HTMLButtonElement | null = document.querySelector('.generate');
 
+const sliderLength: HTMLInputElement | null = document.querySelector('#passwordLength')
+const sliderValue: HTMLOutputElement | null = document.querySelector('output');
+
+
+// i dont even recall why i went for interfaces here, probably some compatibility issues down the line, anyway it does the job
 interface States {
     includeSmall: boolean,
     includeLarge: boolean,
@@ -20,6 +25,7 @@ interface States {
 
 }
 
+// lets me dynamically watch for user options being selected/deselected
 let state: States = {
     includeSmall: false,
     includeLarge: false,
@@ -27,6 +33,7 @@ let state: States = {
     includeSpecial: false
 }
 
+// creates a final string for password gen, based on which options user selects - the string is REMADE every time an option is selected or deselected 
 const updatePassword = () => {
     let finalPassword: string = ''
 
@@ -51,21 +58,24 @@ const updatePassword = () => {
     }
 }
 
+// takes a final string, generated based on user's selections, and creates a string serving as the password from the selected options. not perfect, sometimes will omit a character type despite it being selected (since its randomized) but i'll fix it later
 let generatePassword = () => {
     let possibleChars: string = updatePassword();
     let endResult: string | null = ''
 
-    for(let i = 0; i < 12; i++){
+    let finalLength: number = parseInt(sliderLength.value)
+
+    for(let i = 0; i < finalLength; i++){
         const randomIndex = Math.floor(Math.random()* possibleChars.length)
         endResult += possibleChars[randomIndex]
     }
 
-    
     result.value = endResult
     console.log(endResult.length)
    
 }
 
+// checks for checkboxes below - not perfect, refreshing the page will refresh the booleans to false, but will NOT change the checked state of the checkboxes itself, will fix later
 checkSmall.addEventListener('change', () => {
     state.includeSmall = !state.includeSmall
     console.log(`Small enabled : ${state.includeSmall}`)
@@ -86,6 +96,13 @@ checkSpecial.addEventListener('change', () => {
     console.log(`Special enabled: ${state.includeSpecial}`)
 })
 
+
+// make password come out when button click - throws an error in the console if no options are selected, but the error doesn't stop the program from working (you can select a character for generating a password, and it'll work fine) so error in the console it is for now
 btnGenerate.addEventListener('click', () => {
     generatePassword()
+})
+
+// handle showing values in the output field
+sliderLength.addEventListener('input', () => {
+    sliderValue.value = sliderLength.value
 })
